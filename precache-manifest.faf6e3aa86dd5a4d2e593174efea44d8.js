9401b183f2c0aaf1de101d9cb79385b10fad3e60
Zarress <jaroslaw.main@gmail.com>
2019-07-23 16:49:17 +0200

    Updates

---
+++ precache-manifest.faf6e3aa86dd5a4d2e593174efea44d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7faf43e679e7fa36e0416fd2a90a770",
+    "url": "/Zarress/test-repository/index.html"
+  },
+  {
+    "revision": "5fca90523916b5933a34",
+    "url": "/Zarress/test-repository/static/css/main.677a9ed5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Zarress/test-repository/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5fca90523916b5933a34",
+    "url": "/Zarress/test-repository/static/js/main.b5bea418.chunk.js"
+  },
+  {
+    "revision": "8d55ef50018475028cad",
+    "url": "/Zarress/test-repository/static/js/runtime~main.ef96da4e.js"
+  },
+  {
+    "revision": "59db9ac01899f55b136c475228ab0930",
+    "url": "/Zarress/test-repository/static/media/2.59db9ac0.jpg"
+  },
+  {
+    "revision": "6a4abb37982a16686a0c2c50aa8e99d5",
+    "url": "/Zarress/test-repository/static/media/20170917_132534.6a4abb37.jpg"
+  },
+  {
+    "revision": "31155cfbcd54d4941025a3033cabc052",
+    "url": "/Zarress/test-repository/static/media/20181116_131124.31155cfb.jpg"
+  },
+  {
+    "revision": "83738b197c1e165c3fdb5ea292b50cdf",
+    "url": "/Zarress/test-repository/static/media/4.83738b19.jpg"
+  },
+  {
+    "revision": "981d6fbbf9780480e66767c372d5712c",
+    "url": "/Zarress/test-repository/static/media/6.981d6fbb.jpg"
+  },
+  {
+    "revision": "dc19ef8249f0683b686d45e4cf949322",
+    "url": "/Zarress/test-repository/static/media/8.dc19ef82.jpg"
+  }
+]);
